af14b865c5de6f354d1dd00502ee4ded66269f23
Rafael Bertelli <rafaelbertelli89@gmail.com>
2017-11-22 23:07:35 -0200

    Routes

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Http } from '@angular/http';
 
 @Component({ // cria um componente do angular
 	moduleId: module.id, // faz com que o templateUrl busque o caminho da propria pasta relativa
@@ -7,18 +6,5 @@ import { Http } from '@angular/http';
 	templateUrl: './app.component.html' // sem o moduleId, devo declarar o caminho absoluto ./app/app.component.html
 })
 export class AppComponent { 
-
-	fotos: Object[] = [];
-	
-	constructor(http: Http) {
-
-		http
-			.get('v1/fotos')
-			.map( res => res.json() )
-			.subscribe( 
-				fotos => this.fotos = fotos,
-				error => console.log(error)
-			)
-	}
 
 }
--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -5,10 +5,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FotoModule } from './foto/foto.module';
 import { HttpModule } from '@angular/http';
+import { PainelModule } from './painel/painel.module';
+import { CadastroComponent } from './cadastro/cadastro.component' // Chamei o component ao invez do module pq soh vou usar o cadastro na pagina da app exclusivamente, nao vou usar em outros pontos, ou seja, ele nao tem o modulo cadastro.module, pois ele faz parte apenenas do app.module
+import { ListagemComponent } from './listagem/listagem.component'
+import { routing } from './app.routes';
+
 
 @NgModule({ // transforma a classe num modulo do angular
-	imports: [ BrowserModule, FotoModule, HttpModule ], // Sao os modulos que meu app.component importa para usar. Sobre o BrowserModule, como roda no navegador, importo o BrowserModule, se fosse no Ionic, seria outro 
-	declarations: [ AppComponent ], // declara o que faz parte deste modulo, neste caso, AppComponent
-	bootstrap: [ AppComponent ] // diz qual o componente que dever ser primeiro iniciado
+	imports: [ // Sao os modulos que meu app.component importa para usar. Sobre o BrowserModule, como roda no navegador, importo o BrowserModule, se fosse no Ionic, seria outro 
+		BrowserModule, 
+		FotoModule, 
+		HttpModule, 
+		PainelModule, 
+		routing 
+	], 
+	declarations: [ // declara o que faz parte deste modulo, neste caso, AppComponent e o CadastroComponent, pois sao componentes exclusivos deste modulo
+		AppComponent, 
+		CadastroComponent, 
+		ListagemComponent 
+	],
+	bootstrap: [ // diz qual o componente que dever ser primeiro iniciado
+		AppComponent 
+	] 
 })
 export class AppModule { }
