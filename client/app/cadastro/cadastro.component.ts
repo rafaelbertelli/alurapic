bf2f932b684a98732fac1a388cac812a7510a047
Rafael Bertelli <rafaelbertelli89@gmail.com>
2017-12-03 18:53:16 -0200

    Envio de dados para o servidor

--- client/app/cadastro/cadastro.component.ts
+++ client/app/cadastro/cadastro.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
-import { FotoComponent } from '../foto/foto.component'
+import { FotoComponent } from '../foto/foto.component';
+import { Http, Headers } from '@angular/http';
 
 @Component({
 	moduleId: module.id,
@@ -8,12 +9,27 @@ import { FotoComponent } from '../foto/foto.component'
 })
 export class CadastroComponent { 
 	foto: FotoComponent = new FotoComponent();
+	http: Http;
+
+	constructor(http: Http) {
+		this.http = http;
+		
+	}
 
 	cadastrar(event) {
 		event.preventDefault();
 
-		console.log(this.foto);
-		
+		let header = new Headers();
+		header.append('Content-Type', 'application/json');
+
+		this.http
+			.post('v1/fotos', JSON.stringify(this.foto),{ headers: header })
+			.subscribe(() => { 
+				this.foto = new FotoComponent();
+				console.log('Foto salva com sucesso!');
+
+			}, erro => console.log(erro));
+			
 	}
 
 }
